5a26972662c65dbf5a31aa89957e3549488c5207
aditya <aditya.rouss@gmail.com>
2026-02-06 07:20:48 +0530

    adding the add product end point and its image end point

--- Ecom-project/src/main/java/com/example/Ecom_project/controller/productController.java
+++ Ecom-project/src/main/java/com/example/Ecom_project/controller/productController.java
@@ -1,46 +1,101 @@
 package com.example.Ecom_project.controller;
 
+
 import com.example.Ecom_project.model.Product;
 import com.example.Ecom_project.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
-import java.util.Optional;
+//import java.util.Optional;
 
 //@CrossOrigin
 @RestController
 @RequestMapping("/api")  // by default it is @GetMapping()
-@CrossOrigin(origins = "http://localhost:5173")
+//@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin("*")
 public class productController {
 
     @Autowired
     private ProductService service;
 
-    @GetMapping("/products")
-    public List<Product> getAllProducts(){
-        return service.getAllProducts();
-    }
-
-//    // ResponseEntity : it is a good practice to sent response with data
 //    @GetMapping("/products")
-//    public ResponseEntity<List<Product>> getAllProducts(){
-//        return new ResponseEntity<>(service.getAllProducts(), HttpStatus.FOUND);
+//    public List<Product> getAllProducts(){
+//
+//        return service.getAllProducts();
 //    }
 
-    @GetMapping("/product/{id}")
-    public Optional<Product> getProducts(@PathVariable int id){
-
-        return service.getProductById(id);
-
-//        return ResponseEntity<>(service.getProductById(id), HttpStatus.OK);
-//        Optional<Product> prd = service.getProductById(id);
-//
-//        if(prd.isPresent())    // another method we can use map
-//        return new ResponseEntity<>(prd, HttpStatus.OK);
-//        else
-//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    // ResponseEntity : it is a good practice to sent response with data
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getAllProducts(){
+        return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
     }
+
+//    @GetMapping("/product/{id}")
+//    public Optional<Product> getProducts(@PathVariable int id){
+//
+//        return service.getProductById(id);
+//
+////        return ResponseEntity<>(service.getProductById(id), HttpStatus.OK);
+////        Optional<Product> prd = service.getProductById(id);
+////
+////        if(prd.isPresent())    // another method we can use map
+////        return new ResponseEntity<>(prd, HttpStatus.OK);
+////        else
+////            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//    }
+
+@GetMapping("/product/{id}")
+public ResponseEntity<Product> getProduct(@PathVariable int id) {
+    Product product = service.getProduct(id);
+    if (product != null) {
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    } else {
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
+
+    //@RequestBody accepts whole object or json as object
+    //@RequestPart accept in different parts
+    // ? because we are not sure either it is data or exception
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product,
+                                        @RequestPart MultipartFile imageFile){
+
+        System.out.println("Hey product recieved !");
+
+        try {
+            Product product1 = service.addProduct(product, imageFile);
+            return new ResponseEntity<>(product1, HttpStatus.CREATED);
+        }
+        catch(Exception e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+    }
+//
+//    @PostMapping("/product")
+//    public void addProduct(@RequestBody Product product, @RequestBody MultipartFile imageFile){
+//
+//            service.addProduct(product, imageFile);
+//    }
+
+    // we are getting image in byte formate
+    @GetMapping("product/{productId}/image")
+    public ResponseEntity<byte[]> getImageByProductId(@PathVariable int productId)
+    {
+        Product product = service.getProduct(productId);
+        byte[] imageFile = product.getImageData();
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.valueOf(product.getImageType()))
+                .body(imageFile);
+
+
+    }
+
 }
--- Ecom-project/src/main/java/com/example/Ecom_project/service/ProductService.java
+++ Ecom-project/src/main/java/com/example/Ecom_project/service/ProductService.java
@@ -4,7 +4,9 @@ import com.example.Ecom_project.model.Product;
 import com.example.Ecom_project.repo.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,10 +21,24 @@ public class ProductService {
         return repo.findAll();
     }
 
-    // Optional<Product> means product may exist or may not exist
-    public Optional<Product> getProductById(int id) {
-        return repo.findById(id);   // this will return us optional product(null or product)
-        // method use function as product but return repo.findById(id).orElse(new Product());   empty product else
+//    // Optional<Product> means product may exist or may not exist
+//    public Optional<Product> getProductById(int id) {
+//        return repo.findById(id);   // this will return us optional product(null or product)
+//        // method use function as product but return repo.findById(id).orElse(new Product());   empty product else
+//
+//    }
+
+    public Product getProduct(int id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageData(imageFile.getBytes());
+
+        return repo.save(product);
 
     }
 }
